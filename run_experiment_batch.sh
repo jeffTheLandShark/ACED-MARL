d136994cd91910851f43747430f898e3fe3059ed
goetschm <luna.bob.mg@gmail.com>
2025-12-14 12:52:13 -0600

    fix batch pathing so that results save correctly

--- run_experiment_batch.sh
+++ run_experiment_batch.sh
@@ -8,16 +8,21 @@
 #SBATCH --array=0-3
 
 # change this to your script path
-SCRIPT_DIR="/home/ad.msoe.edu/goetschm/CSC5661/final-project"
-experiment_path="${SCRIPT_DIR}/ACED-MARL/src/train_mappo.py"
-config_dir="${SCRIPT_DIR}/ACED-MARL/src/configs"
+SCRIPT_DIR="."
+experiment_path="${SCRIPT_DIR}/src/train_mappo.py"
+config_dir="${SCRIPT_DIR}/src/configs"
 
 # Config permutations
 configs=(
+  # "quick_test.yaml"         # Quick test
   "mappo_sync.yaml"         # MAPPO baseline
   "mappo_event.yaml"        # MAPPO + event driven
   "mappo_atoc_sync.yaml"    # MAPPO + ATOC
   "mappo_atoc_event.yaml"   # MAPPO + ATOC + event driven
+  # "mat_sync.yaml"           # MAT baseline
+  # "mat_event.yaml"          # MAT + event driven
+  # "mat_atoc_sync.yaml"      # MAT + ATOC
+  # "mat_atoc_event.yaml"     # MAT + ATOC + event driven
 )
 
 cfg="${configs[$SLURM_ARRAY_TASK_ID]}"
